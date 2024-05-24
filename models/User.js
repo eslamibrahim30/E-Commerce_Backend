5c55feae794ab013188cf21d7d0cd8a713373281
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-25 01:32:16 +0300

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
+import { isEmail } from 'validator';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, 'name required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'email required'],
+    unique: true,
+    validate: [ isEmail, 'not valid email'],
+    },
+  password: {
+    type: String,
+    required: [true, 'Password required'],
+    minlength: [6, 'at laset > 6'],
+  },
   shippingAddress: {
     street: { type: String },
     city: { type: String },
@@ -13,5 +26,15 @@ const userSchema = new mongoose.Schema({
   },
   phoneNumber: { type: String },
 });
-
+// hashing Password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
 module.exports = mongoose.model('User', userSchema);
