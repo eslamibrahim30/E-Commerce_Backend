0edda4c00e7dd502377eb5ade810f74c5651cca0
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-28 18:05:33 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authcheck = require('../middlewares/auth');
-const { createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { authcheck } = require('../middlewares/auth'); 
+const { createUser, updateUser, deleteUser, getUsers, loginUser } = require('../controllers/userController');
 
-router.post('/users', async () => {
-  authcheck, createUser
-});
-router.put('/update', async () => {
-  authcheck, updateUser
-});
-router.delete('/delete', async (req, res) => {
-  authcheck, deleteUser;
-});
+router.post('/users', authcheck, createUser);
+router.post('/login', authcheck, loginUser);
+router.get('/users', authcheck, getUsers);
+router.put('/update', authcheck, updateUser);
+router.delete('/delete', authcheck, deleteUser);
+
 module.exports = router;
