b0c55b6d3ad3a56216d7993a1e0bd5c8d4265712
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-28 18:05:13 +0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+const user = { id: '' };
+const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
 exports.authcheck = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, '');
+    // const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.userData = { userId: decodedToken.userId };
     next();
   } catch {
