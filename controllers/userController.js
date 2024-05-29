7bfb9abacb0d3b59196620931c4150d7f531a793
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-30 02:17:32 +0300

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,10 @@
 const User = require('../models/User');
+const { Token } = require('../models/Token');
 const { authcheck } = require('../middlewares/auth');
 const bcrypt = require('bcryptjs');
 
-// const handlserrors = (error) => {
-//   if (error.code === 11000) {
-//     rerurn('email Already exit')
-//   }
-//   throw(error.message);
-// };
-
 // Create a new user
 const createUser = async (req, res) => {
-  if (authcheck) {
     const { name, email, password } = req.body;
     if (!email) {
       res.status(400).json({ error: 'Missing email' });
@@ -28,11 +21,7 @@ const createUser = async (req, res) => {
     }
     const user = new User({ name, email, password });
     await user.save();
-    res.status(200).json({ id: user._id, email: user.email, name: user.name});
-    }
-  //   else (error) => {
-  //     handlserrors(error);
-  // }
+  res.status(200).json({ id: user._id, email: user.email, name: user.name});
 };
 
 // Get all users
@@ -41,78 +30,60 @@ const getUsers = async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ error: handlserrors(error) });
+    res.status(500).json({ error: (error) });
   }
 };
 
 // login
 const loginUser = async (req, res) => {
-  if (authcheck) {
-    const { name, email, password } = req.body;
-    if (!name || !password || !email) {
-      return res.status(400).json({
-        message: 'missing data',
-      })
-    }
-    const loginuser = await User.findOne({ email }).select('password');
-    if (!loginuser) {
-      res.status(400).json({ error: 'User not found' });
-    };
-    if (loginuser) {
-      const comp = await bcrypt.compare(password, loginuser.password);
-      if (comp) {
-        res.status(200).json({ message: 'Login successful', loginuser });
-      } else {
-        res.status(400).json({ message: 'password not match' });
-      }
-    }
-
+  const { name, email, password } = req.body;
+  if (!name || !password || !email) {
+    return res.status(400).json({
+    message: 'missing data',
+    });
+  }
+  const loginuser = await User.findOne({ email }).select('password');
+  if (!loginuser) {
+    res.status(400).json({ error: 'User not found' });
   };
-  
+  if (loginuser) {
+    const comp = await bcrypt.compare(password, loginuser.password);
+    if (comp) {
+      res.status(200).json({ message: 'Login successful', loginuser});
+      console.log(await loginuser.generateAuthToken());
+    } else {
+      res.status(400).json({ message: 'password not match' });
+    }
+  }
 };
-  //  else {
-  //   handlserrors(error);
-  // }
 
 
 // Update user by ID
 const updateUser = async (req, res) => {
-  if (authcheck) {
-    const { _id, email,newData } = req.body
-    if (!_id) {
-      return res.status(400).json({ error: 'User ID and email are required' });
-    }
-    const user = await User.findOneAndUpdate({ _id, email}, newData, { new: true });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    return res.status(200).json(user);
+  const { _id, email,newData } = req.body;
+  if (!_id) {
+    return res.status(400).json({ error: 'User ID are required' });
   }
-  //  catch (error) {
-  //   handlserrors(error);
-  // }
+  const user = await User.findOneAndUpdate({ _id, email}, newData, { new: true });
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+  return res.status(200).json(user);
 };
 
 // Delete user by ID
 const deleteUser = async (req, res) => {
-  if(authcheck) {
-    const { _id, email } = req.body
-    if (!_id || !email) {
-      return res.status(400).json({ error: 'User ID and email are required' });
-    }
-    const deluser = await User.findOne({ _id, email });
-    if (!deluser) {
-      return res.status(400).json({ error: 'User not found' });
-    };
-    if (deluser) {
-      await User.deleteOne(deluser);
-      res.status(201).send({ message: 'User successfully deleted' })
-    };    
-    // .catch(error =>
-    //   res
-    //     .status(400)
-    //     .json({ message: 'An error occurred', error: error.message })
-    // )
+  const { _id, email } = req.body
+  if (!_id || !email) {
+    return res.status(400).json({ error: 'User ID and email are required' });
+  }
+  const deluser = await User.findOne({ _id, email });
+  if (!deluser) {
+    return res.status(400).json({ error: 'User not found' });
+  };
+  if (deluser) {
+    await User.deleteOne(deluser);
+    res.status(201).send({ message: 'User deleted' })
   };
 };
 
