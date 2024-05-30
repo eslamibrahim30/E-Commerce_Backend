c6c5d23041359553e0a8dc9364d408b136e32f1d
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-30 03:38:02 +0300

    Fixed importing issue

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Cart = require('../models/Cart');
-const Product = require('../models/BaseProduct');
+const Order = require('../models/Order');
 
 // Create order
 const createOrder = async (req, res) => {
