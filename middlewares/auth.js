5c52660d0ffda6a52674dabddbb812a2800e3f34
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-30 23:11:19 +0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { Token } = require('../models/Token');
 
-exports.authcheck = async (req, res, next) => {
+const authcheck = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ error: 'Header not provided' });
+    return res.status(401).json({ error: 'Header not provided or not admin' });
   }
   const token = authHeader.split(' ')[1];
   if (!token) {
@@ -19,26 +19,14 @@ exports.authcheck = async (req, res, next) => {
     }
     if (tokenDoc) {
       const isMatch = await bcrypt.compare(token, tokenDoc.token);
+      // console.log(isMatch);
       if (!isMatch) {
         return res.status(401).json({ error: 'Invalid token not matched' });
       }
       req.verified = verified;
+      // console.log(verified.role);
       next();
     }
-    // const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    // const tokenDoc = await Token.findOne({ userId: decodedToken._id });
-
-    // if (!tokenDoc || tokenDoc.expiresAt < new Date()) {
-    //   return res.status(401).json({ error: 'Invalid or expired token' });
-    // }
-
-    // const isMatch = await bcrypt.compare(token, tokenDoc.token);
-    // if (!isMatch) {
-    //   return res.status(401).json({ error: 'Invalid token' });
-    // }
-
-    // req.user = decodedToken;
-    // next();
   } catch(err) {
     if (err instanceof jwt.TokenExpiredError) {
       return res.status(401).json({ error: 'Token expired' });
@@ -49,3 +37,15 @@ exports.authcheck = async (req, res, next) => {
     }
   }
 };
+
+const verifyAdmin = (req, res, next) => {
+  authcheck (req, res, () => {
+    if (req.verified.role === 'admin') {
+      next();
+    } else {
+      return res.status(401).json({ error: 'You are not authorized as Admin!'});
+    }
+  });
+};
+
+module.exports = { authcheck, verifyAdmin };
